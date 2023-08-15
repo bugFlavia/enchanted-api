9ac2a3b3910d175eb992de6ae6b8f225333700b7
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-08-15 15:06:36 -0400

    lista

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,7 +1,12 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const usuarios = [
+    {name:"Flavia", email:"verdade@concordo", password:"renatao", token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"},
+    {name:"Renato", email:"renatao@calvo.minoxidil", password:"ciso", token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw8F"}
+]
 
+const getUserAuthenticated = (user) => {
+  
 }
 
 const getUsers = () =>{
