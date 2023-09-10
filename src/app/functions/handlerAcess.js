23c5c3fb6cc73c323fde385bf08b0aa7cac4daa9
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-10 16:47:38 -0400

    localStorage

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -1,17 +1,17 @@
 import Cookies from "js-cookie";
 import { validateToken } from "./validateToken";
-import { getUserAuthenticated } from "./handlerAcessAPI";
-
+import { getUserAuthenticated} from "./handlerAcessAPI";
 const handlerAcessUser = async (user) => {
 
-    const userAuth = await getUserAuthenticated(user);
+    const {userAuth, nomeUsuario} = await getUserAuthenticated(user);
     
     const isTokenValidate = validateToken(userAuth.token);
 
     if (isTokenValidate) {
         Cookies.set('token', userAuth.token, { expires: 1 });
+        localStorage.setItem('nomeUsuario', nomeUsuario);
     }
-    return userAuth;
+    return {userAuth, nomeUsuario};
 }
 export default handlerAcessUser;
 
--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -5,15 +5,19 @@ const usuarios = [
     {name:"Renato", email:"renatao@calvo.minoxidil", password:"ciso", token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw8F"}
 ]
 
+
 const getUserAuthenticated = (user) => {
     let userAuth = {};
+    let nomeUsuario = ''
     usuarios.map((e) => {
         if(e.email === user.email && e.password === user.password){
             userAuth = e;
+            nomeUsuario = e.name
         };
     })
-    return userAuth;
+    return {userAuth, nomeUsuario};
 }
+
 
 const getUsers = () =>{
         return usuarios
