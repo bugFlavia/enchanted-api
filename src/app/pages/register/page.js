2ad3095e90bf579064e29fc1984132e04b3b5df8
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-10 19:35:21 -0400

    alerts

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,11 +1,20 @@
+'use client'
+
+import { ToastContainer, toast } from "react-toastify";
+
 export default function Register() {
+    const ficaLigado = () => {
+        toast.success("Dados enviados!");
+    }
     return (
         <div>
-            <form method="post">
+            <form method="post" onSubmit={ficaLigado}>
                 <label>Nome:</label><input type="text" placeholder="Digite seu Nome" name="nome"/>
                 <label>E-mail:</label><input type="text" placeholder="Digite seu E-mail" name="email"/>
-                <label>Senha:</label><input type="text" placeholder="Digite uma senha" name="senha"/>          
+                <label>Senha:</label><input type="text" placeholder="Digite uma senha" name="senha"/> 
+                <button>Enviar</button>         
             </form>
+            <ToastContainer/>
         </div>
     );
 };
