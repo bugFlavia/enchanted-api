884dcbaea7d72d4a1e97e2ab5d88e486a1c0bb9c
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-10 19:38:36 -0400

    middleware(feito no comit "rotas")

--- src/middleware.js
+++ src/middleware.js
@@ -11,6 +11,7 @@ export const middleware = (request) => {
     if (!isTokenValidated || !token) {
         if (request.nextUrl.pathname === '/pages/dashboard' || request.nextUrl.pathname==='/pages/register' || request.nextUrl.pathname==='/pages/alter') {
             return NextResponse.redirect(urlLogin);
+            console.log("Página indisponível");
         }
     }
     NextResponse.next();
