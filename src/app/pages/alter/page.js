606ebc518836f8c232c612a8f5001c98bc64e165
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-16 20:59:35 -0400

    update_alert

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -3,12 +3,12 @@
 import { ToastContainer, toast } from "react-toastify";
 
 export default function Alter() {
-        const ficaLigado = () => {
+        const enviado = () => {
             toast.success("Dados enviados!");
         }
     return (
         <div>
-            <form onSubmit={ficaLigado}>
+            <form onSubmit={enviado}>
                 <label>Nome:</label><input type="text" placeholder="Digite seu Nome" name="nome"/>
                 <label>E-mail:</label><input type="text" placeholder="Digite seu E-mail" name="email"/>
                 <label>Senha:</label><input type="text" placeholder="Digite uma senha" name="senha"/> 
--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -3,12 +3,12 @@
 import { ToastContainer, toast } from "react-toastify";
 
 export default function Register() {
-    const ficaLigado = () => {
+    const enviado = () => {
         toast.success("Dados enviados!");
     }
     return (
         <div>
-            <form method="post" onSubmit={ficaLigado}>
+            <form method="post" onSubmit={enviado}>
                 <label>Nome:</label><input type="text" placeholder="Digite seu Nome" name="nome"/>
                 <label>E-mail:</label><input type="text" placeholder="Digite seu E-mail" name="email"/>
                 <label>Senha:</label><input type="text" placeholder="Digite uma senha" name="senha"/> 
