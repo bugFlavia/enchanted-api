d337f080cd43c2ec7b5636f3d6df4c7028337cc1
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-16 22:40:38 -0400

    Create rodape.js

---
+++ src/app/componentes/rodape.js
@@ -0,0 +1,18 @@
+'use client'
+import Link from 'next/link';
+import {Copyright} from 'lucide-react'
+
+export default function Rodape(){
+    return(
+
+        <footer className='grid grid-cols-2 pt-5'>
+                <div className='gap-4'>
+                   <h1 className="font-bold text-2xl">Criado por:</h1>
+                    <h2 className='font-semibold text-lg'>Flavia Nunes Barboza de Morais.</h2>
+                </div>
+            
+                
+            
+        </footer>
+    )
+}
