178038edfc4239917269c2a792af60829338fb1d
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-09-17 00:56:57 -0400

    update rodape e nav

--- src/app/componentes/navbar.js
+++ src/app/componentes/navbar.js
@@ -7,10 +7,10 @@ export default function Navbar(){
 
         <nav>
             <ul>
-                <li><Link href="/dashboard" id="rota">HOME</Link></li>
-                <li><Link href="/register" id="rota">CADASTRAR</Link></li>
-                <li><Link href="/alter" id='rota'>Alterar</Link></li>
-                <li><Link href="/" id='rota'>Login</Link></li>
+                <li><Link href="/pages/dashboard" id="rota">HOME</Link></li>
+                <li><Link href="/pages/register" id="rota">CADASTRAR</Link></li>
+                <li><Link href="/pages/alter" id='rota'>ALTERAR</Link></li>
+                <li><Link href="/" id='rota'>LOGIN</Link></li>
                 <p id='pesquisa'><input type='search' placeholder='Pesquisar'/><button><Search/></button></p>
             </ul>
             
--- src/app/componentes/rodape.js
+++ src/app/componentes/rodape.js
@@ -10,7 +10,10 @@ export default function Rodape(){
                    <h1 className="font-bold text-2xl">Criado por:</h1>
                     <h2 className='font-semibold text-lg'>Flavia Nunes Barboza de Morais.</h2>
                 </div>
-            
+                <div className='gap-4'>
+                    <p className='font-semibold text-lg'>Direitos de imagem reservados à:</p>
+                    <p className='flex gap-2'><Copyright/><a className='font-medium text-purple-400 dark:text-purple-400 hover:underline' href='https://taylorswiftbr.com/index.php'> Taylor Swift Brasil </a></p>
+                </div>
                 
             
         </footer>
