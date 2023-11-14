7736ed3dfab8f3cce14fe3fa466673455355eaa8
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-11-14 14:05:04 -0400

    verdade

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -2,9 +2,9 @@
 import Fallback from "@/app/componentes/fallback";
 import Navbar from "@/app/componentes/navbar";
 import { postUser } from "@/app/functions/handlerAcessAPI";
-import React, {useState} from 'react'
 import Rodape from "@/app/componentes/rodape";
-import { Suspense, useState } from "react";
+import { React, Suspense, useState } from "react";
+import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -13,44 +13,47 @@ export default function Register() {
         name: '',
         email: '',
         password: ''
-    })
-    const {push} = useRouter();
+    });
+
     const enviado = () => {
         toast.success("Dados enviados!");
-    }
+    };
 
-    const handlerFormSubmit = async (event) =>{
+    const handlerFormSubmit = async (event) => {
         event.preventDefault();
-        try{
+        try {
             await postUser(user);
             await new Promise((resolve) => {
                 toast.success("Usuário cadastrado com sucesso!");
                 setTimeout(resolve, 5000);
-            })
+                enviado(); // Corrected function call
+            });
             return push("/pages/dashboard");
-        }catch{
-            return toast.error("Erro")
+        } catch {
+            return toast.error("Erro");
         }
     };
+
     return (
         <div>
             <Suspense fallback={<Fallback />}>
-        <div className="geral">
-        <Navbar/>
-           <div className='mt-20 pb-24 geral'>
-            <form method="post"  className='flex flex-col ml-auto mr-auto w-1/2 bg-white p-10 gap-2 rounded-lg' onSubmit={enviado}>
+                <div className="geral">
+                    <Navbar />
+                    <div className='mt-20 pb-24 geral'>
+                        <form method="post" className='flex flex-col ml-auto mr-auto w-1/2 bg-white p-10 gap-2 rounded-lg' onSubmit={handlerFormSubmit}>
 
-              <h1>Registre um usuário:</h1>
+                            <h1>Registre um usuário:</h1>
 
-                <input type="text" placeholder="Digite seu Nome" name="nome" required/>
-                <input type="email" placeholder="Digite seu E-mail" name="email" required/>
-                <input type="password" placeholder="Digite uma senha" name="senha" required/> 
-                <button className="botao">Enviar</button>      
-            </form>
-            <ToastContainer /></div>
-            <Rodape/>
-        </div>
-        </Suspense>
+                            <input type="text" placeholder="Digite seu Nome" name="name" required /> {/* Corrected name */}
+                            <input type="email" placeholder="Digite seu E-mail" name="email" required />
+                            <input type="password" placeholder="Digite uma senha" name="password" required /> {/* Corrected name */}
+                            <button className="botao">Enviar</button>
+                        </form>
+                        <ToastContainer />
+                    </div>
+                    <Rodape />
+                </div>
+            </Suspense>
         </div>
     );
-};
+}
